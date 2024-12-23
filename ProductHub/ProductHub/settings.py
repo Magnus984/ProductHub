d86ea685a2af4f165451091710e2ee0086fd0e51
Magnus <nharnharyhaw984@gmail.com>
2024-12-23 12:59:09 +0000

    Configure database settings to use environment variables and add initial migration files for cart, orders, products, and users

--- ProductHub/ProductHub/settings.py
+++ ProductHub/ProductHub/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'producthub',
-        'USER': 'root',
-        'PASSWORD': 'windowsql',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     },
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
