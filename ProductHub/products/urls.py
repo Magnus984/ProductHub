7c291eb56f1a886fda8370d4a994992d07873bc8
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-12-24 12:08:04 +0000

    Altered products urls after including in root urls

--- ProductHub/products/urls.py
+++ ProductHub/products/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # Product URLs
-    path('products/', views.product_list, name='product-list'),
-    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+    path('', views.product_list, name='product-list'),
+    path('<int:pk>/', views.product_detail, name='product-detail'),
 
     # Category URLs
-    path('categories/', views.category_list, name='category-list'),
-    path('categories/<int:pk>/', views.category_detail, name='category-detail'),
+    path('/categories/', views.category_list, name='category-list'),
+    path('/categories/<int:pk>/', views.category_detail, name='category-detail'),
 
     # Review URLs
-    path('reviews/', views.review_list, name='review-list'),
-    path('reviews/<int:pk>/', views.review_detail, name='review-detail'),]
+    path('/reviews/', views.review_list, name='review-list'),
+    path('/reviews/<int:pk>/', views.review_detail, name='review-detail'),]
