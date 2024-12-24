abb5b576b4895f9c8ece455eb6edba4346483d6b
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-12-24 11:09:53 +0000

    Added orders views, serializers and urls files

---
+++ ProductHub/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Order URLs
+    path('orders/', views.order_list, name='order-list'),
+    path('orders/<int:pk>/', views.order_detail, name='order-detail'),
+
+    # OrderItem URLs
+    path('order-items/', views.order_item_list, name='order-item-list'),
+    path('order-items/<int:pk>/', views.order_item_detail, name='order-item-detail'),
+]
