b507cb877dd46579e0a23a98f2f5cb4db579559a
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-12-24 12:09:24 +0000

    Altered cart urls after including in root urls

--- ProductHub/cart/urls.py
+++ ProductHub/cart/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     # Cart URLs
-    path('carts/', views.cart_list, name='cart-list'),
-    path('carts/<int:pk>/', views.cart_detail, name='cart-detail'),
+    path('', views.cart_list, name='cart-list'),
+    path('/<int:pk>/', views.cart_detail, name='cart-detail'),
 
     # CartItem URLs
-    path('carts/<int:cart_id>/items/', views.cart_item_list, name='cart-item-list'),
-    path('carts/<int:cart_id>/items/<int:pk>/', views.cart_item_detail, name='cart-item-detail'),
+    path('/carts/<int:cart_id>/items/', views.cart_item_list, name='cart-item-list'),
+    path('/carts/<int:cart_id>/items/<int:pk>/', views.cart_item_detail, name='cart-item-detail'),
 ]
