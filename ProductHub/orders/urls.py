a366f4580969296d531db8e61e91c7c0d61cd004
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2024-12-24 12:08:48 +0000

    Altered orders urls after including in root urls

--- ProductHub/orders/urls.py
+++ ProductHub/orders/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     # Order URLs
-    path('orders/', views.order_list, name='order-list'),
-    path('orders/<int:pk>/', views.order_detail, name='order-detail'),
+    path('', views.order_list, name='order-list'),
+    path('/<int:pk>/', views.order_detail, name='order-detail'),
 
     # OrderItem URLs
-    path('order-items/', views.order_item_list, name='order-item-list'),
-    path('order-items/<int:pk>/', views.order_item_detail, name='order-item-detail'),
+    path('/order-items/', views.order_item_list, name='order-item-list'),
+    path('/order-items/<int:pk>/', views.order_item_detail, name='order-item-detail'),
 ]
