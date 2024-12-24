1093ea76958f1c45b19d1be6950625cd441b18ac
Magnus <nharnharyhaw984@gmail.com>
2024-12-24 21:52:19 +0000

    Add URL routing for current user view

--- ProductHub/users/urls.py
+++ ProductHub/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterAdminView, RegisterCustomerView, LoginView, LogoutView
+from .views import RegisterAdminView, RegisterCustomerView, LoginView, LogoutView, GetCurrentUserView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register-customer/', RegisterCustomerView.as_view(), name='register-customer'),
     path('register-admin/', RegisterAdminView.as_view(), name='register-admin'),
+    path('current-user/', GetCurrentUserView.as_view(), name='current-user'),
 ]
