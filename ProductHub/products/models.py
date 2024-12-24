4903272839eae698dd9d67bed9034b3fea3dced6
Magnus <nharnharyhaw984@gmail.com>
2024-12-24 21:06:17 +0000

    Add image field to Product model for product image uploads

--- ProductHub/products/models.py
+++ ProductHub/products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/products/')
 
     class Meta:
         db_table = 'product'
