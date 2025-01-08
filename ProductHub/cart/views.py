ed3617f0600e596e1003ec5f03141f7ae050733c
Henry Agyemang Duah <henry.agyemang@amalitech.com>
2025-01-08 10:50:55 +0000

    Refeactored views to make use of the exceptions and util functions for error handling

--- ProductHub/cart/views.py
+++ ProductHub/cart/views.py
@@ -1,15 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
+from django.db import transaction
 from .models import Cart, CartItem
 from .serializers import CartSerializer, CartItemSerializer
-from products.models import Product
+from .exceptions import CartItemNotFoundException
+from .utils import handle_cart_exceptions, validate_cart_item_quantity, validate_product
+
 
 class CartView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @handle_cart_exceptions
     def get(self, request):
         """Get user's cart or create if doesn't exist"""
         cart, created = Cart.objects.get_or_create(
@@ -21,84 +23,83 @@ class CartView(APIView):
 class CartItemView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @handle_cart_exceptions
+    @transaction.atomic
     def post(self, request):
         """Add item to cart"""
+
+        product_id = request.data.get('product_id')
+        product = validate_product(product_id)
+        
+        quantity = validate_cart_item_quantity(request.data.get('quantity', 1))
+        
         cart, created = Cart.objects.get_or_create(
             customer_id=request.user.customer
         )
 
-        product_id = request.data.get('product_id')
-        quantity = int(request.data.get('quantity', 1))
+        cart_item = CartItem.objects.filter(
+            cart_id=cart,
+            product_id=product.id
+        ).first()
 
-        if quantity <= 0:
-            return Response(
-                {'error': 'Quantity must be greater than 0'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        try:
-            cart_item = CartItem.objects.get(
-                cart_id=cart,
-                product_id=product_id
-            )
+        if cart_item:
             cart_item.quantity += quantity
             cart_item.save()
-        except CartItem.DoesNotExist:
+        else:
             CartItem.objects.create(
                 cart_id=cart,
-                product_id_id=product_id,
+                product_id=product,
                 quantity=quantity
             )
 
         serializer = CartSerializer(cart)
         return Response(serializer.data)
 
+    @handle_cart_exceptions
+    @transaction.atomic
     def put(self, request, item_id):
         """Update cart item quantity"""
-        try:
-            cart_item = CartItem.objects.get(
-                id=item_id,
-                cart_id__customer_id=request.user.customer
-            )
-            quantity = int(request.data.get('quantity', 1))
+        quantity = validate_cart_item_quantity(request.data.get('quantity'))
+        
+        cart_item = CartItem.objects.filter(
+            id=item_id,
+            cart_id__customer_id=request.user.customer
+        ).first()
 
-            if quantity <= 0:
-                cart_item.delete()
-            else:
-                cart_item.quantity = quantity
-                cart_item.save()
+        if not cart_item:
+            raise CartItemNotFoundException()
 
-            serializer = CartSerializer(cart_item.cart_id)
-            return Response(serializer.data)
+        validate_product(cart_item.product_id.id)
 
-        except CartItem.DoesNotExist:
-            return Response(
-                {'error': 'Cart item not found'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        cart_item.quantity = quantity
+        cart_item.save()
 
+        serializer = CartSerializer(cart_item.cart_id)
+        return Response(serializer.data)
+
+    @handle_cart_exceptions
+    @transaction.atomic
     def delete(self, request, item_id):
         """Remove item from cart"""
-        try:
-            cart_item = CartItem.objects.get(
-                id=item_id,
-                cart_id__customer_id=request.user.customer
-            )
-            cart = cart_item.cart_id
-            cart_item.delete()
+        cart_item = CartItem.objects.filter(
+            id=item_id,
+            cart_id__customer_id=request.user.customer
+        ).first()
+        
+        if not cart_item:
+            raise CartItemNotFoundException()
 
-            serializer = CartSerializer(cart)
-            return Response(serializer.data)
+        cart = cart_item.cart_id
+        cart_item.delete()
 
-        except CartItem.DoesNotExist:
-            return Response(
-                {'error': 'Cart item not found'},
-                status=status.HTTP_404_NOT_FOUND
-            )
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
 
 class ClearCartView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @handle_cart_exceptions
+    @transaction.atomic
     def post(self, request):
         """Clear all items from cart"""
         cart = Cart.objects.get(customer_id=request.user.customer)
