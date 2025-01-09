9913b7b4567ab933bd03cd9d95ebc711e31f9a8b
Magnus <nharnharyhaw984@gmail.com>
2025-01-09 18:13:52 +0000

    Update cart URL patterns to use new views for adding items and item details

--- ProductHub/cart/urls.py
+++ ProductHub/cart/urls.py
@@ -1,10 +1,10 @@
 # cart/urls.py
 from django.urls import path
-from .views import CartView, CartItemView, ClearCartView
+from .views import CartView, AddCartItemView, CartItemDetailView, ClearCartView
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('items/', CartItemView.as_view(), name='cart-items'),
-    path('items/<int:item_id>/', CartItemView.as_view(), name='cart-item-detail'),
+    path('items/', AddCartItemView.as_view(), name='cart-items'),
+    path('items/<int:item_id>/', CartItemDetailView.as_view(), name='cart-item-detail'),
     path('clear/', ClearCartView.as_view(), name='clear-cart'),
 ]
