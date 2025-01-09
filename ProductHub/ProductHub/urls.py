61fc61200d5c70fd63562cb609eb907edb19cf15
Magnus <nharnharyhaw984@gmail.com>
2025-01-09 18:15:29 +0000

    Add Swagger UI endpoint and custom token views for API documentation

--- ProductHub/ProductHub/urls.py
+++ ProductHub/ProductHub/urls.py
@@ -1,33 +1,53 @@
-"""
-URL configuration for ProductHub project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""URL configuration for ProductHub project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ProductHub API",
+      default_version='v1',
+      description="Product hub is an e-commerce-like application programming interface.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      #contact=openapi.Contact(email="contact@snippets.local"),
+      #license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=[]
+)
+
+class CustomTokenObtainPairView(jwt_views.TokenObtainPairView):
+    @swagger_auto_schema(tags=['Auth'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+class CustomTokenRefreshView(jwt_views.TokenRefreshView):
+    @swagger_auto_schema(tags=['Auth'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('api/products/', include('products.urls')),
-    path('api/cart/', include('cart.urls')),
-    path('api/orders/', include('orders.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/',
+        include([
+            path('products/', include('products.urls')),
+            path('cart/', include('cart.urls')),
+            path('orders/', include('orders.urls')),
+            path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+            path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+        ])
+        ),
 ]
 
 if settings.DEBUG:
