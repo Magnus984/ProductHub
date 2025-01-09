d6dcd55bceb62616b3c79ab1693663c84397a401
Magnus <nharnharyhaw984@gmail.com>
2025-01-09 18:14:53 +0000

    Add Swagger documentation for user registration, cart operations, and order management views

--- ProductHub/users/views.py
+++ ProductHub/users/views.py
@@ -8,11 +8,28 @@ from django.contrib.auth import get_user_model
 from .models import CustomUser, Customer, Admin
 from .serializers import CustomUserSerializer, CustomerSerializer, AdminSerializer
 from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 User = get_user_model()
 
 class RegisterCustomerView(APIView):
     """Register customer view."""
+    @swagger_auto_schema(
+        operation_description="Registers a customer",
+        tags=['Users'],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "username": openapi.Schema(type=openapi.TYPE_STRING, description="Username"),
+                "firstname": openapi.Schema(type=openapi.TYPE_STRING, description="Firstname"),
+                "lastname": openapi.Schema(type=openapi.TYPE_STRING, description="Lastname"),
+                "password": openapi.Schema(type=openapi.TYPE_STRING, description="Password"),
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="Email"),
+                "residential_address": openapi.Schema(type=openapi.TYPE_STRING, description="residential_address"),
+            }
+            )
+    )
     def post(self, request):
         serializer = CustomerSerializer(data=request.data)
         try:
@@ -25,6 +42,20 @@ class RegisterCustomerView(APIView):
 
 class RegisterAdminView(APIView):
     """Register admin view."""
+    @swagger_auto_schema(
+        operation_description="Registers an admin",
+        tags=['Users'],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "username": openapi.Schema(type=openapi.TYPE_STRING, description="Username"),
+                "firstname": openapi.Schema(type=openapi.TYPE_STRING, description="Firstname"),
+                "lastname": openapi.Schema(type=openapi.TYPE_STRING, description="Lastname"),
+                "password": openapi.Schema(type=openapi.TYPE_STRING, description="Password"),
+                "email": openapi.Schema(type=openapi.TYPE_STRING, description="Email"),
+            }
+            )
+    )
     def post(self, request):
         serializer = AdminSerializer(data=request.data)
         try:
@@ -42,6 +73,10 @@ class GetCurrentUserView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Gets current logged-in user",
+        tags=['Users']
+    )
     def get(self, request):
         user = request.user
         serializer = CustomUserSerializer(user)
